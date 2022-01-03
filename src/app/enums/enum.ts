f517bcbe315e5998b50fb1cdeb175b223637e761
danny alenjandro <dannysenju@gmail.com>
2022-01-03 11:12:44 -0500

    Added enum RolesEnum Added funcionability roles navbar

--- src/app/enums/enum.ts
+++ src/app/enums/enum.ts
@@ -6,3 +6,11 @@ export enum LocalStorageKeyEnum {
   token="token",
   type="type",
 }
+
+export  enum RolesEnum {
+  COORD_GENERAL = "COORD_GENERAL",
+  COORD_TEAM = "COORD_TEAM",
+  SUPERINTENDENT = "SUPERINTENDENT",
+  PSYCHOLOGIST = "PSYCHOLOGIST",
+  SOCIAL_WORKER = "SOCIAL_WORKER"
+}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,22 +8,50 @@ import {LocalStorage} from "../../storage/local-storage";
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  @Input() fullName: string = "";
+  fullName: string = "";
   oLocalStorage = new LocalStorage();
+  buttonMenuCoord = false;
+  buttonMenuCoordTeam = false;
+  buttonMenuSuperIntendent = false;
+  buttonMenuPsychoLogist = false;
+  buttonMenuSocialWorker = false;
 
 
   constructor() {
     this.fullName = this.oLocalStorage.getItem(LocalStorageKeyEnum.name) + " " + this.oLocalStorage.getItem(LocalStorageKeyEnum.lastName);
-    if (LocalStorageKeyEnum.userName === null){
-      
-    }
+    this.typeRole(this.oLocalStorage.getItem(LocalStorageKeyEnum.rol));
   }
-
   logOut() {
     this.oLocalStorage.removeItem(LocalStorageKeyEnum.rol);
     this.oLocalStorage.removeItem(LocalStorageKeyEnum.lastName);
     this.oLocalStorage.removeItem(LocalStorageKeyEnum.name);
     this.oLocalStorage.removeItem(LocalStorageKeyEnum.type);
     this.oLocalStorage.removeItem(LocalStorageKeyEnum.token);
+
+    this.buttonMenuCoord = false;
+    this.buttonMenuCoordTeam = false;
+    this.buttonMenuSuperIntendent = false;
+    this.buttonMenuPsychoLogist = false;
+    this.buttonMenuSocialWorker = false;
+  }
+
+  private typeRole(rol: any) {
+    switch (rol){
+      case "COORD_GENERAL":
+        this.buttonMenuCoord = true;
+        break;
+      case "COORD_TEAM":
+        this.buttonMenuCoordTeam = true;
+        break;
+      case "SUPERINTENDENT":
+        this.buttonMenuSuperIntendent = true;
+        break;
+      case "PSYCHOLOGIST":
+        this.buttonMenuPsychoLogist = true;
+        break;
+      case "SOCIAL_WORKER":
+        this.buttonMenuSocialWorker = true;
+        break;
+    }
   }
 }
