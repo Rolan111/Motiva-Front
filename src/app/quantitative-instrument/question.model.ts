f720a906a36cf49c5ebe97cf18c88c7b236dcfb0
Nestor Agredo <nestor.agredol@campusucc.edu.co>
2022-02-08 22:11:12 -0500

    - stable code

--- src/app/quantitative-instrument/question.model.ts
+++ src/app/quantitative-instrument/question.model.ts
@@ -1,15 +1,17 @@
-export class QuestionModel {
+  export class QuestionModel {
   id_question: number;
   description: string;
   id_father: number;
   id_question_type: number;
   order: number;
+  type: string;
 
-  constructor(id_question: number, description: string, id_father: number, id_question_type: number, order: number) {
+  constructor(id_question: number, description: string, id_father: number, id_question_type: number, order: number, type: string) {
     this.id_question = id_question;
     this.description = description;
     this.id_father = id_father;
     this.id_question_type = id_question_type;
     this.order = order;
+    this.type = type;
   }
 }
