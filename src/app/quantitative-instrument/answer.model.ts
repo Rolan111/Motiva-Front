44dc959ba3bc3e3d84d484071bab1ef5caa644e8
Nestor Agredo <nestor.agredol@campusucc.edu.co>
2022-02-21 13:11:03 -0500

    - Improved Code

--- src/app/quantitative-instrument/answer.model.ts
+++ src/app/quantitative-instrument/answer.model.ts
@@ -4,12 +4,14 @@ export class AnswerModel {
   idOptionAnswers: Array<number>[];
   openAnswer: string;
   idPoll: number;
+  type: string;
 
-  constructor(idAnswer: number, idQuestion: number, idOptionAnswers: Array<number>[], openAnswer: string, idPoll: number) {
+  constructor(idAnswer: number, idQuestion: number, idOptionAnswers: Array<number>[], openAnswer: string, idPoll: number, type: string) {
     this.idAnswer = idAnswer;
     this.idQuestion = idQuestion;
     this.idOptionAnswers = idOptionAnswers;
     this.openAnswer = openAnswer;
     this.idPoll = idPoll;
+    this.type = type;
   }
 }
