f658a22ae6a47ede342e3e62a20c787ebe19fdee
Rolan <estibenruiz05@gmail.com>
2022-04-11 12:57:01 -0500

    Improve code

--- src/app/alerts/alerts.module.ts
+++ src/app/alerts/alerts.module.ts
@@ -3,19 +3,22 @@ import {CommonModule} from '@angular/common';
 import {AlertsComponent} from "./alerts.component";
 import {AngularMaterialModule} from "../angular-material.module";
 import {HttpClientModule} from "@angular/common/http";
+import {InstrumentReviewComponent} from "./instrument-review/instrument-review.component";
 
 
 @NgModule({
   declarations: [
-    AlertsComponent
+    AlertsComponent,
+    InstrumentReviewComponent
   ],
   exports: [
-    AlertsComponent
+    AlertsComponent,
+    InstrumentReviewComponent
   ],
   imports: [
     CommonModule,
     AngularMaterialModule,
-    HttpClientModule
+    HttpClientModule,
   ]
 })
 export class AlertsModule {
