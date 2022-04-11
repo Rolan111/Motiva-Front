4a00086acc2c2218146586f3f3ca531f73bcce84
Ruby Meneses <menesesalbeniz@gmail.com>
2022-04-11 12:41:18 -0500

    Add module care-rasm

---
+++ src/app/care-rasm/care-rasm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-care-rasm',
+  templateUrl: './care-rasm.component.html',
+  styleUrls: ['./care-rasm.component.scss']
+})
+export class CareRasmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
