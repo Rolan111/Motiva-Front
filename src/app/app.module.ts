ec57cdf23f6a968754d2698a10d519b4ca149485
Rolan <estibenruiz05@gmail.com>
2022-04-11 12:57:01 -0500

    Improve code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,10 @@ import {CareRoutesModule} from "./care-routes/care-routes.module";
 import {QuantitativeInstrumentsModule} from "./quantitative-instruments/quantitative-instruments.module";
 import {CareSheetModule} from "./care-sheet/care-sheet.module";
 import {AlertsModule} from "./alerts/alerts.module";
-import {InstrumentReviewComponent} from './alerts/instrument-review/instrument-review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InstrumentReviewComponent,
   ],
   imports: [
     BrowserModule,
