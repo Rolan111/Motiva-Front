90319a7ff2d65d7ae85c710a3a0c46bf87fe1ebd
Nestor Agredo <nestor.agredol@campusucc.edu.co>
2022-04-17 22:01:18 -0500

    -Improved answer model

--- src/app/quantitative-instruments/answer.model.ts
+++ src/app/quantitative-instruments/answer.model.ts
@@ -5,13 +5,16 @@ export class AnswerModel {
   openAnswer: string;
   idPoll: number;
   type: string;
+  score: number;
 
-  constructor(idAnswer: number, idQuestion: number, idOptionAnswers: Array<number>[], openAnswer: string, idPoll: number, type: string) {
+  constructor(idAnswer: number, idQuestion: number, idOptionAnswers: Array<number>[], openAnswer: string, idPoll: number,
+              type: string, score: number) {
     this.idAnswer = idAnswer;
     this.idQuestion = idQuestion;
     this.idOptionAnswers = idOptionAnswers;
     this.openAnswer = openAnswer;
     this.idPoll = idPoll;
     this.type = type;
+    this.score = score;
   }
 }
