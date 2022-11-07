1cbf8d6f30f9e1aee284fb11edccba4069e9e936
oscardacto16 <oscar.dacto@gracialab.com.co>
2022-11-07 13:04:39 -0500

    DAct dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ export interface UserData {
   id: string;
   name: string;
   progress: string;
-  fruit: string;
+  atenciones: string;
 }
 
 /** Constants used to fill up our data base. */
@@ -21,49 +21,50 @@ const FRUITS: string[] = [
   'pomegranate',
   'pineapple',
 ];
-const NAMES: string[] = [
-  'Buenos Aires',
-  'Caloto',
-  'Corinto',
-  'Guachené',
-  'Miranda',
-  'Padilla',
-  'Puerto Tejada',
-  'Santander de Quilichao',
-  'Suárez',
-  'Villa Rica',
-  'Cajibío',
-  'El Tambo',
-  'La Sierra',
-  'Morales',
-  'Piendamó',
-  'Popayán',
-  'Rosas',
-  'Sotará',
-  'Timbío',
-  'Almaguer',
-  'Argelia',
-  'Balboa',
-  'Bolívar',
-  'Florencia',
-  'La Vega',
-  'Mercaderes',
-  'Patía',
-  'Piamonte',
-  'San Sebastián',
-  'Santa Rosa',
-  'Sucre',
-  'Guapi',
-  'López de Micay',
-  'Timbiquí',
-  'Caldono',
-  'Inzá	',
-  'Jambaló',
-  'Páez',
-  'Puracé',
-  'Silvia',
-  'Toribío',
-  'Totoró',
+const MUNICIPIOS: string[] = [
+    '',
+  'POPAYAN' ,
+  'ALMAGUER' ,
+  'ARGELIA' ,
+  'BALBOA' ,
+  'BOLIVAR' ,
+  'BUENOS AIRES' ,
+  'CAJIBIO' ,
+  'CALDONO' ,
+  'CALOTO' ,
+  'CORINTO' ,
+  'EL TAMBO' ,
+  'FLORENCIA' ,
+  'GUACHENE' ,
+  'GUAPI' ,
+  'INZA' ,
+  'JAMBALO' ,
+  'LA SIERRA' ,
+  'LA VEGA' ,
+  'LOPEZ' ,
+  'MERCADERES' ,
+  'MIRANDA' ,
+  'MORALES' ,
+  'PADILLA' ,
+  'PAEZ' ,
+  'PIAMONTE' ,
+  'PIENDAMO' ,
+  'PUERTO TEJADA' ,
+  'PATIA' ,
+  'PURACE' ,
+  'ROSAS' ,
+  'SAN SEBASTIAN' ,
+  'SANTANDER DE QUILICHAO' ,
+  'SANTA ROSA' ,
+  'SILVIA' ,
+  'SOTARA' ,
+  'SUAREZ' ,
+  'SUCRE' ,
+  'TIMBIO' ,
+  'TIMBIQUI' ,
+  'TORIBIO' ,
+  'TOTORO' ,
+  'VILLA RICA'
 ];
 
 @Component({
@@ -72,7 +73,7 @@ const NAMES: string[] = [
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  displayedColumns: string[] = ['id', 'name',  'atenciones'];
   dataSource: MatTableDataSource<UserData>;
 
   // @ts-ignore
@@ -81,7 +82,7 @@ export class DashboardComponent implements AfterViewInit {
 
   constructor() {
     // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    const users = Array.from({length: MUNICIPIOS.length-1}, (_, k) => createNewUser(k + 1));
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
@@ -104,16 +105,14 @@ export class DashboardComponent implements AfterViewInit {
 
 /** Builds and returns a new User. */
 function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
+  const num_atenciones = Math.round(Math.random() * 1429);
 
+  // @ts-ignore
   return {
     id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+    name: MUNICIPIOS[id],
+    atenciones: num_atenciones.toString(),
+    progress: Math.round((num_atenciones*100)/1429).toString()
+
   };
 }
