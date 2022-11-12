edbfbd1d3ff69f238a686f3bc16b1b9970716de9
Rolan <estibenruiz05@gmail.com>
2022-11-12 06:59:29 -0500

    mejora environment en producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,14 @@ export const environment = {
   production: true,
   url: 'http://3.88.39.15:5000/',
   // url: 'https://3.88.39.15:5000/',
+  firebaseConfig: {
+    apiKey: "AIzaSyBtDlWk0rma9zmMPjcXB0u_DWvhKh2cKZo",
+    authDomain: "motiva-db.firebaseapp.com",
+    databaseURL: "https://motiva-db-default-rtdb.firebaseio.com",
+    projectId: "motiva-db",
+    storageBucket: "motiva-db.appspot.com",
+    messagingSenderId: "757090170968",
+    appId: "1:757090170968:web:320267b93e24b68c77b22d",
+    measurementId: "G-RPZVNNSV0K"
+  },
 };
