eb9565ced250cbf6b70f2d7b08463c4b8c47e1ec
Rolan <estibenruiz05@gmail.com>
2023-01-17 10:15:53 -0500

    Mejoras OVA

--- src/app/care-sheet/care-sheet.module.ts
+++ src/app/care-sheet/care-sheet.module.ts
@@ -3,15 +3,18 @@ import {CommonModule} from '@angular/common';
 import {AngularMaterialModule} from "../angular-material.module";
 import {CareSheetComponent} from "./care-sheet.component";
 import {MatStepperModule} from "@angular/material/stepper";
+import {OVAComponent} from "../ova/ova.component";
 
 
 
 @NgModule({
   declarations: [
-    CareSheetComponent
+    CareSheetComponent,
+    OVAComponent
   ],
   exports: [
-    CareSheetComponent
+    CareSheetComponent,
+    OVAComponent
   ],
   imports: [
     CommonModule,
